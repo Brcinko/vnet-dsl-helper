b3cd50f4e2d5b493424907e1606b4c09c987a052
Brcinko <lukas.klescinec@gmail.com>
2017-12-03 13:05:50 -0600

    again first call

--- backend/xml-generator.py
+++ backend/xml-generator.py
@@ -1,6 +1,18 @@
 import settings
 import file_helper
+import dicttoxml
+import json
 
 
+test_dict = {}
+test_dict['Product'] = 'ADSL Partner Naked L'
+test_dict['Contact'] = {}
+test_dict['Contact']['PhoneNumber'] = '0902123456'
+test_dict['Contact']['LastName'] = 'Andrejovic'
+test_dict['Contact']['FirstName'] = 'Andrej'
+test_dict['Address'] = None
 
+# obj = json.dumps(test_dict)
+xml = dicttoxml.dicttoxml(test_dict, custom_root='Request', attr_type=False , )
 
+print xml
